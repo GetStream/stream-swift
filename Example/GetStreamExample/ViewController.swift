6931801b9393aeecc3acd37e40049ab4f1ba6632
Alexey Bukhtin <bukhtin@gmail.com>
2018-11-08 10:55:54 +0100

    Added an example project. Feed endpoint added.

---
+++ Example/GetStreamExample/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  GetStreamExample
+//
+//  Created by Alexey Bukhtin on 07/11/2018.
+//  Copyright © 2018 Alexey Bukhtin. All rights reserved.
+//
+
+import UIKit
+import GetStream
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- Sources/GetStream.swift
+++ Sources/GetStream.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+import Moya
+import Require
+
+public final class Client {
+    
+    open static var location: Client.Location = .usEast
+    
+    public static var baseURL: URL {
+        return URL(string: "https://\(location.rawValue)api.stream-io-api.com/api/v1.0/").require()
+    }
+    
+    private let moyaProvider = MoyaProvider<MultiTarget>()
+    
+    /// The API key, it can be safely shared with untrusted entities.
+    private let apiKey: String
+    
+    /// A reference to the application id in Stream. This is only used for realtime notifications.
+    private let appId: String
+    
+    /// The API secret, it is used to generate the feed tokens.
+    private let secretKey: String?
+    
+    init(apiKey: String, appId: String, secretKey: String? = nil, location: Client.Location = .usEast) {
+        self.apiKey = apiKey
+        self.appId = appId
+        self.secretKey = secretKey
+        Client.location = location
+    }
+}
+
+extension Client {
+    public enum Location: String {
+        case usEast = "us-east-"
+        case europeWest = "eu-west-"
+        case singapore = "singapore-"
+    }
+}
