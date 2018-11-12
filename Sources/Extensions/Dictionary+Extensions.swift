41c564069d8ef1c7db1936f287c97f574115deab
Alexey Bukhtin <bukhtin@gmail.com>
2018-11-12 10:56:42 +0100

    Cleanup the code.

---
+++ Sources/Extensions/Dictionary+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  Dictionary+Extensions.swift
+//  GetStream
+//
+//  Created by Alexey Bukhtin on 12/11/2018.
+//  Copyright © 2018 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+
+/// Creates a dictionary by merging the given dictionary into this
+/// dictionary, replacing values with values of the other dictionary.
+extension Dictionary {
+    func merged(with other: Dictionary) -> Dictionary {
+        return merging(other, uniquingKeysWith: { _, new in new })
+    }
+}
