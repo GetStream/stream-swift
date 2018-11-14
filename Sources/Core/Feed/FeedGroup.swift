7078a5eb01cd26bbe576067854aae5157b66c57e
Alexey Bukhtin <bukhtin@gmail.com>
2018-11-14 11:35:24 +0100

    Add activity to several feeds.

--- Sources/Core/Feed/FeedGroup.swift
+++ Sources/Core/Feed/FeedGroup.swift
@@ -8,18 +8,45 @@
 
 import Foundation
 
-public struct FeedGroup {
+public struct FeedGroup: CustomStringConvertible, Codable {
+    private static let separator: Character = ":"
+    
     /// The name of the feed group, for instance user, trending, flat, timeline etc. For example: flat, timeline.
     let feedSlug: String
     /// The owner of the given feed.
     let userId: String
+    
     /// The feed group id. E.g. `timeline:123`
-    var id: String {
-        return feedSlug.appending(":").appending(userId)
+    public var description: String {
+        return feedSlug.appending(String(FeedGroup.separator)).appending(userId)
     }
     
     public init(feedSlug: String, userId: String) {
         self.feedSlug = feedSlug
         self.userId = userId
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let id = try container.decode(String.self)
+        
+        if id.isEmpty {
+            throw DecodingError.dataCorruptedError(in: container,
+                                                   debugDescription: "Cannot initialize FeedGroup from an empty string")
+        }
+        
+        let pair = id.split(separator: FeedGroup.separator).map { String($0) }
+        
+        if pair.count != 2 {
+            throw DecodingError.dataCorruptedError(in: container,
+                                                   debugDescription: "Cannot initialize FeedGroup from a currupted string: \(id)")
+        }
+        
+        self.init(feedSlug: pair[0], userId: pair[1])
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(description)
     }
 }
