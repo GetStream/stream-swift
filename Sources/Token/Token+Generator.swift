c6421584c77a3cac005d7f87bdc42d9b5262deff
Alexey Bukhtin <bukhtin@gmail.com>
2018-11-15 13:23:19 +0100

    Updated Token generator parameters.

--- Sources/Token/Token+Generator.swift
+++ Sources/Token/Token+Generator.swift
@@ -11,13 +11,19 @@ import SwiftyJWT
 
 extension Token {
     /// Generate the Stream token with a given secret.
-    public init?(secret: String, resource: String = "*", permission: Permission = .all, feedId: FeedId? = nil) {
+    ///
+    /// - Parameters:
+    ///     - secret: a secret string.
+    ///     - resource: a resource string, e.g. feed
+    ///     - permission: a permissionm e.g. read or write
+    ///     - feedId: a `FeedId` or any as by default.
+    public init?(secret: String, resource: Resource = .all, permission: Permission = .all, feedId: FeedId = .any) {
         let algorithm = JWTAlgorithm.hs256(secret)
         var payload = JWTPayload()
         
-        payload.customFields = ["resource": EncodableValue(value: resource),
+        payload.customFields = ["resource": EncodableValue(value: resource.rawValue),
                                 "action": EncodableValue(value: permission.rawValue),
-                                "feed_id": EncodableValue(value: feedId?.feedSlug.appending(feedId?.userId ?? "") ?? "*")]
+                                "feed_id": EncodableValue(value: feedId.description)]
         
         let jwt = JWT(payload: payload, algorithm: algorithm)
         
@@ -30,10 +36,26 @@ extension Token {
 }
 
 extension Token {
+    public enum Resource: String {
+        /// Allow access to any resource.
+        case all = "*"
+        /// Activities Endpoint.
+        case activities
+        /// Feed Endpoint.
+        case feed
+        /// Following + Followers Endpoint.
+        case follower
+    }
+}
+extension Token {
     public enum Permission: String {
         case all = "*"
         case read
         case write
         case delete
     }
+}
+
+extension FeedId {
+    public static let any = FeedId(feedSlug: "*", userId: "")
 }
