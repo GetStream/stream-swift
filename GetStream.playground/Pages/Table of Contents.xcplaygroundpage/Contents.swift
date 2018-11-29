75d87e8afa87350c19c9346d3abfba2138750a8b
Alexey Bukhtin <bukhtin@gmail.com>
2018-11-29 16:18:09 +0100

    Added playground.

---
+++ GetStream.playground/Pages/Table of Contents.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+/*:
+ # Stream
+ ## Scalable news feeds & activity streams
+ ### Learn how easy it can be to build scalable newsfeeds and activity streams
+ 
+ Stream has official clients for JS/Node, Ruby, Python, PHP, Go and Java. There are framework integrations available for Rails, Django, Laravel, Doctrine, Zend and Node. In addition to the official clients, the community has built clients for .NET and Scala.
+ 
+ - [Quick start](Quick%20start)
+ 
+ ****
+ [Next](@next)
+ */
