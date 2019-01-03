32b0be4c873a4556e443f512bbe0a7e3f9a02d28
Alexey Bukhtin <bukhtin@gmail.com>
2019-01-03 16:20:43 +0100

    Package updated.

--- Package.swift
+++ Package.swift
@@ -7,18 +7,15 @@ let package = Package(
     name: "GetStream",
     products: [
         .library(name: "GetStream", targets: ["GetStream"]),
-        .library(name: "GetStreamToken", targets: ["GetStreamToken"]),
         .library(name: "Faye", targets: ["Faye"]),
     ],
     dependencies: [
         .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "12.0.0")),
         .package(url: "https://github.com/antitypical/Result.git", .upToNextMajor(from: "4.1.0")),
-        .package(url: "https://github.com/kylef/JSONWebToken.swift.git", .upToNextMajor(from: "2.2.0")),
         .package(url: "https://github.com/daltoniam/Starscream.git", .upToNextMajor(from: "3.0.0")),
     ],
     targets: [
-        .target(name: "GetStream", dependencies: ["Moya", "JWT"], path: "Sources/Core"),
-        .target(name: "GetStreamToken", dependencies: ["GetStream"], path: "Sources/Token"),
+        .target(name: "GetStream", dependencies: ["Moya", "Faye"], path: "Sources/Core"),
         .target(name: "Faye", dependencies: ["Starscream"], path: "Faye"),
         .testTarget(name: "GetStreamTests", dependencies: ["GetStream"], path: "Tests/Core"),
     ],
