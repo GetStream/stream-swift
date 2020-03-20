e59e2e5a557140bfcf8e0b08e8c4a895040c0606
Bahadir Oncel <bahadir.onc@gmail.com>
2020-03-20 11:08:33 +0100

    Update SPM, Cocoapods and Carthage specs

--- Package.swift
+++ Package.swift
@@ -1,24 +1,25 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "GetStream",
+    platforms: [
+        .iOS(.v11)
+    ],
     products: [
         .library(name: "GetStream", targets: ["GetStream"]),
         .library(name: "Faye", targets: ["Faye"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "12.0.0")),
-        .package(url: "https://github.com/antitypical/Result.git", .upToNextMajor(from: "4.1.0")),
+        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "14.0.0")),
         .package(url: "https://github.com/daltoniam/Starscream.git", .upToNextMajor(from: "3.0.0")),
         .package(url: "https://github.com/sendyhalim/Swime", .upToNextMajor(from: "3.0.0")),
     ],
     targets: [
-    .target(name: "GetStream", dependencies: ["Moya", "Faye", "Swime", "Result"], path: "Sources", exclude: ["Token"]),
+        .target(name: "GetStream", dependencies: ["Moya", "Faye", "Swime"], path: "Sources", exclude: ["Token"]),
         .target(name: "Faye", dependencies: ["Starscream"], path: "Faye"),
         .testTarget(name: "GetStreamTests", dependencies: ["GetStream"], path: "Tests", exclude: ["Token"]),
-    ],
-    swiftLanguageVersions: [.v4_2]
+    ]
 )
